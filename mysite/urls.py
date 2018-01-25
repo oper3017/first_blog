83a4c25f711b88d9732212f386a3a4d1889bc5fb
oper3017 <oper3017@24dcaea7d349>
2018-01-25 02:30:43 +0000

    url modify

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
+    url(r'', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
